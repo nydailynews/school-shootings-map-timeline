933bf79fb1c7e950e0a6adc95ee7da4069c5e242
freejoe76 <jmurphy@denverpost.com>
2018-02-15 12:56:30 -0500

    Initial commit

---
+++ source.bash
@@ -0,0 +1,2 @@
+alias deployqa='cd ~/work/mass-shootings-since-newtown/www/; scp -r . qa:/apps/map/mass-shootings-since-sandy-hook/'
+alias deployprod='cd ~/work/mass-shootings-since-newtown/www/; scp -r . prod:/apps/map/mass-shootings-since-sandy-hook/'
