7dbac5db2652573b8b10e7061b276311d8588844
Joe Murphy <joe.murphy@gmail.com>
2018-02-21 12:11:55 -0500

    Delete source.bash

--- source.bash
+++
@@ -1,2 +0,0 @@
-alias deployqa='cd ~/work/mass-shootings-since-newtown/www/; scp -r . qa:/apps/map/mass-shootings-since-sandy-hook/'
-alias deployprod='cd ~/work/mass-shootings-since-newtown/www/; scp -r . prod:/apps/map/mass-shootings-since-sandy-hook/'
